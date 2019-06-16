578c18849ac1bf4c5df63c3efda56b3ce055f7c6
Chris Wilding <hello@chriswilding.co.uk>
2019-06-16 19:32:42 +0100

    Apply scalafmtAll and use scalafmtCheckAll on CI (#20)

--- test/controllers/GraphQLControllerSpec.scala
+++ test/controllers/GraphQLControllerSpec.scala
@@ -5,11 +5,11 @@ import org.scalatestplus.play.guice._
 import play.api.test._
 
 /**
- * Add your spec here.
- * You can mock out a whole application including requests, plugins etc.
- *
- * For more information, see https://www.playframework.com/documentation/latest/ScalaTestingWithScalaTest
- */
+  * Add your spec here.
+  * You can mock out a whole application including requests, plugins etc.
+  *
+  * For more information, see https://www.playframework.com/documentation/latest/ScalaTestingWithScalaTest
+  */
 class GraphQLControllerSpec extends PlaySpec with GuiceOneAppPerTest with Injecting {
 
   "GraphQLController GET" should {
