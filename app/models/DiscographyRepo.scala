e4f9e74a764dc0deb537d235a1356617fcf571ec
Chris Wilding <hello@chriswilding.co.uk>
2019-08-03 19:32:11 +0100

    Add album (#33)

--- app/models/DiscographyRepo.scala
+++ app/models/DiscographyRepo.scala
@@ -1,11 +1,18 @@
 package models
 
 import javax.inject.Inject
+import models.album.{Album, AlbumDAO}
 import models.artist.{Artist, ArtistDAO}
 
 import scala.concurrent.Future
 
-class DiscographyRepo @Inject()(artistDAO: ArtistDAO) {
+class DiscographyRepo @Inject()(artistDAO: ArtistDAO, albumDAO: AlbumDAO) {
+  def getAlbum(id: Int): Future[Option[Album]] =
+    albumDAO.lookup(id)
+
   def getArtist(id: Int): Future[Option[Artist]] =
     artistDAO.lookup(id)
+
+  def getByArtist(artistId: Int): Future[Seq[Album]] =
+    albumDAO.getByArtist(artistId: Int)
 }
